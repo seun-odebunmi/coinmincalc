70c725842a9f1da99f4071e11c1a67ec64056f35
Seun Odebunmi <seun_odebunmi@yahoo.com>
2017-07-24 22:42:16 +0100

    updates

--- assets/controller/converter.js
+++ assets/controller/converter.js
@@ -1,6 +1,7 @@
 angular.module("coinConvApp", [])
 .controller("myCoinConvCtrl", function ($scope) {
-
+	
+	//store all possible coins and their value
 	$scope.presetCoins = [
 		{"denomination": "£2", "penceValue": 200},
 		{"denomination": "£1", "penceValue": 100},
@@ -11,6 +12,23 @@ angular.module("coinConvApp", [])
 		{"denomination": "2p", "penceValue": 2},
 		{"denomination": "1p", "penceValue": 1}
 	];
+	
+	//input validation to allow input that starts with £ currency
+	$scope.inputValidation = function (inputAmount) {
+		var amount = inputAmount;
+		
+		if (amount.match(/(?=.)^\£?(([1-9][0-9]{0,2}(,[0-9]{3})*)|0)?(\.[0-9]{1,2})?$/)) {
+			if (amount.charAt(0) === '£') {
+				amount = amount.substr(1) * 100;
+			}
+			$scope.errorMsg = "";
+		}else{
+			amount = 0;
+			$scope.errorMsg = "Please enter a valid amount ex. ($24.50)";
+			$scope.coinCalcForm.amountValue.$invalid = true;
+		}
+		return parseInt(amount);
+	};
 
     // calculate the least number of pennies
 	$scope.minNumCoins = function (inputAmount) {
@@ -27,8 +45,10 @@ angular.module("coinConvApp", [])
     };
 
     $scope.amountValue = "";
+	$scope.errorMsg = "Must be a valid number!";
     $scope.finalCoins = [];
     $scope.calcMinCoin = function () {
-        $scope.finalCoins = $scope.minNumCoins($scope.amountValue);
+		var validAmount = $scope.inputValidation($scope.amountValue);
+        $scope.finalCoins = $scope.minNumCoins(validAmount);
     }
 });
